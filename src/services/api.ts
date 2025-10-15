e3f4ebd2f7b592bd104712f6e0b5f422adc749e6
Franklin García <franklin.garcia60105@uped.edu.sv>
2025-10-15 17:04:37 -0600

    Cambios necesarios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  // Aquí debemos si o si porner la URL del backend que ha hecho Dennis en Flask
+  // No tocar ya, aquí debemos si o si porner la URL del backend en la instancia que ha levantado Dennis en AWS
+  // No olvidar el /api al final
+  // No cambiar a localhost ni a nada, debe ser la IP pública que Dennis ha puesto
+  // --- IGNORE ---
   baseURL: "http://3.129.110.135/api",
   headers: {
     "Content-Type": "application/json",
